1ab84a98faf92b0fe06b21442fea0c8d1b917c1b
surajit <devil@DESKTOP-T443TI5.localdomain>
2022-05-15 16:38:56 +0300

    add payment intent and organization mode

--- stripe_app/admin.py
+++ stripe_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from stripe_app import models
+
+
+@admin.register(models.Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'org_id',)
+
+
+@admin.register(models.PaymentIntent)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('intent_id', 'charge_amount', "organization",)
--- stripe_app/models.py
+++ stripe_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now=True, editable=False, db_index=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Organization(TimeStampedModel):
+    org_id = models.CharField(max_length=120, unique=True, db_index=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Organization Id#{self.org_id} for organization {self.name}'
+
+
+class PaymentIntent(TimeStampedModel):
+    intent_id = models.CharField(max_length=255)
+    charge_amount = models.IntegerField(default=11900)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'PaymentIntent id {self.intent_id} for Organization Name {self.organization.name} '
+
+    class Meta:
+        verbose_name = _("PaymentIntent")
+        verbose_name_plural = _("PaymentIntent")
